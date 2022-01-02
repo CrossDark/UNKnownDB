1b21a5710e350320a2984f066f75dd1811051216
clever <liuhanbo333@icloud.com>
2022-01-02 09:32:38 +0800

    LightDB []

--- UNKnownDB/DB/LightDB.py
+++ UNKnownDB/DB/LightDB.py
@@ -34,10 +34,18 @@ class File:
         write = re.findall('(.+):(.+)', other)
         change = '' + write[0][0] + '' + write[0][1] + ''
         change_list = [(m.group(), m.span()) for m in re.finditer(change, str(self.DB.read(), 'utf-8'))]
-        print(change_list)
         self.DB.seek(change_list[0][1][0])
-        self.DB.write(bytes('dw', 'utf-8'))
+        self.DB.write(bytes(chr(0)*(change_list[0][1][1] - change_list[0][1][0]), 'utf-8'))
         return self
 
     def __setitem__(self, key, value):
         """[]"""
+        if key in str(self.DB.read()):
+            print(9999)
+            change = '' + key + '(.+?)'
+            change_list = [(m.group(), m.span()) for m in re.finditer(change, str(self.DB.read(), 'utf-8'))]
+            self.DB.seek(change_list[0][1][0])
+            self.DB.write(bytes('' + key + '' + value + '', 'utf-8'))
+        else:
+            self.DB.seek(0, 2)
+            self.DB.write(bytes('' + key+ '' + value + '', 'utf-8'))
