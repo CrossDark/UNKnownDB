980783c1d916574bea058c9e0c9a7c1d0791fe5c
clever <liuhanbo333@icloud.com>
2022-01-09 16:43:50 +0800

    pack version num bugs

--- build/lib/UNKnownDB/Test/__init__.py
+++ build/lib/UNKnownDB/Test/__init__.py
@@ -1,12 +1,23 @@
 # import DB
 # from UNKnownDB.UNDL import Interpreter, APP
 from UNKnownDB.DB import LightDB
+from UNKnownDB.DSL import Main
 
 with LightDB.Data('light') as db:
-    del db['try']
-    print(db)
+    for x in db:
+        print(x)
 
 with open('./light.unl') as light:
     print(light.read())
+
+main = Main(
+    'd:ww\nop:200\nf:poi'
+)
+main.interrupt(
+    {
+        'd': lambda y: print(y),
+        'op': lambda z: print(z + '2th')
+    }
+)
 # db = DB.LocalDB("./.Clever.unp")
 # db.create()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='UNKnownDB',
-    version='1.6.2',
+    version='163.0',
     author='CleaverCreator',
     author_email='liuhanbo333@icloud.com',
     packages=find_packages(),
