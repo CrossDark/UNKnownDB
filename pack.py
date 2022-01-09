87fba5b042d35bb65f4df5ca5eccd97c02f19520
clever <liuhanbo333@icloud.com>
2022-01-09 16:53:12 +0800

    pack version num bugs fixed

--- pack.py
+++ pack.py
@@ -1,4 +1,4 @@
-import os
+# import os
 import time
 import re
 from UNKnownDB.DB import LocalDB
@@ -9,7 +9,8 @@ with open('./setup.py', 'r+') as setup:
     find = re.findall('version=(.+?),', read)
     v_id = float(eval(find[0].replace('.', '')))
     setup.seek(0, 0)
-    writing = read.replace(find[0], "'" + str(v_id + 1) + "'")
+    num = str(v_id + 1)
+    writing = read.replace(find[0], "'" + num[0] + '.' + num[1] + '.' + num[2] + "'")
     setup.write(writing[:-1])
 # os.system('python3 setup.py sdist bdist_wheel')
 time.sleep(5)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='UNKnownDB',
-    version='163.0',
+    version='1.6.2',
     author='CleaverCreator',
     author_email='liuhanbo333@icloud.com',
     packages=find_packages(),
